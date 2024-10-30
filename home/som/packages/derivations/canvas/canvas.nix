1998994b1ffcf72466dd190a2555d5f3951d5aa2
somokill <somokill@noreply.codeberg.org>
2024-10-30 21:11:27 +0500

    f

--- home/som/packages/derivations/canvas/canvas.nix
+++ home/som/packages/derivations/canvas/canvas.nix
@@ -1,20 +1,10 @@
-{ pkgs, ... }: pkgs.stdenv.mkDerivation rec {
-  pname = "canvas";
-  version = "3.0";
+{ pkgs, fetchurl, ... }: pkgs.writeShellScriptBin {
+  name = "canvas";
 
-  src = pkgs.fetchFromGitHub {
-    owner = "adi1090x";
-    repo = "${pname}";
-    rev = "b4b1e3456d86db98052bd7735ae3c0a8799b1061";
-    hash = "sha256-yyZjwU3n1UJfVQ05if3oKBNKfjURmvfHFe2zC91Zu8g=";
+  text = fetchurl {
+    url = "https://github.com/adi1090x/canvas/blob/master/canvas";
+    hash = "";
   };
 
   runtimeInputs = with pkgs; [ imagemagick feh xcolor xorg.xrandr ];
-
-  phases = [ "installPhase" ];
-  installPhase = ''
-    mkdir -p $out/bin
-    cp $src/canvas $out/bin
-    chmod +x $out/bin/canvas
-  '';
 }
