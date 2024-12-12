f6a818fbd5baef0f068d5c3e9baebf57c9271da8
somokill <somokill@noreply.codeberg.org>
2024-12-12 22:32:58 +0500

    Some changes (auto)

--- home/som/dooit/default.nix
+++ home/som/dooit/default.nix
@@ -3,8 +3,7 @@
     inputs.dooit.homeManagerModules.default
   ];
 
-  # nixpkgs.overlays = [ inputs.dooit-extras.overlay ];
-
+  nixpkgs.overlays = [ inputs.dooit-extras.overlay ];
   programs.dooit = {
     enable = true;
     extraPackages = [ pkgs.dooit-extras ];
