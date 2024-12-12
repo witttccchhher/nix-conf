e3f0bfc4dd081d3f36d634dd240f9868dc25fc78
somokill <somokill@noreply.codeberg.org>
2024-12-12 18:34:45 +0500

    Some changes (auto)

--- home/som/mpv/default.nix
+++ home/som/mpv/default.nix
@@ -9,6 +9,6 @@
   };
   xdg.configFile."mpv/scripts/modernx.lua".text = builtins.readFile(pkgs.fetchurl {
     url = "https://raw.githubusercontent.com/cyl0/ModernX/refs/heads/main/modernx.lua";
-    hash = pkgs.lib.fakeSha256;
+    hash = "";
   });
 }
