a26f24d0843e13be9c16053f122712e4deb8b3b5
somokill <somokill@noreply.codeberg.org>
2024-12-13 09:18:42 +0500

    Some changes (auto)

--- home/som/dooit/default.nix
+++ home/som/dooit/default.nix
@@ -2,6 +2,8 @@
   mydooit = inputs.dooit.packages.${pkgs.system}.default.override {
     extraPackages = [
       inputs.dooit-extras.packages.${pkgs.system}.default
+      pkgs.python311Packages.tkinter
+      pkgs.tk
     ];
   };
 in {
