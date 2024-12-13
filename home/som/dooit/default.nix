320a232b0148bf026369ccddf0b453f786007b46
somokill <somokill@noreply.codeberg.org>
2024-12-13 11:09:07 +0500

    Some changes (auto)

--- home/som/dooit/default.nix
+++ home/som/dooit/default.nix
@@ -1,5 +1,5 @@
 { inputs, pkgs, ... }: let
-  mydooit-extras = inputs.dooit-extras.packages.${pkgs.system}.default.override {
+  mydooit-extras = inputs.dooit-extras.packages.${pkgs.system}.default.overrideAttrs {
     propagatedBuildInputs = [ pkgs.python311Packages.tkinter ];
   };
   mydooit = inputs.dooit.packages.${pkgs.system}.default.override {
