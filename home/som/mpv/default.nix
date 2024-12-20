9b9a068c65009729ab7a1cd1f8fbfa4280167058
somokill <somokill@noreply.codeberg.org>
2024-12-20 17:10:38 +0500

    Some changes (auto)

--- home/som/mpv/default.nix
+++ home/som/mpv/default.nix
@@ -3,12 +3,12 @@
     enable = true;
     config = {
       player-operation-mode = "pseudo-gui";
-      osc = "no";
-      border = "no";
+      # osc = "no";
+      # border = "no";
     };
   };
-  xdg.configFile."mpv/scripts/modernx.lua".text = builtins.readFile(pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/cyl0/ModernX/refs/heads/main/modernx.lua";
-    hash = "sha256-Vfd6bWu8z7RqB8HzuKctxM9/AHAN7/P4KHQ4IRXGx4Y=";
-  });
+  # xdg.configFile."mpv/scripts/modernx.lua".text = builtins.readFile(pkgs.fetchurl {
+  #   url = "https://raw.githubusercontent.com/cyl0/ModernX/refs/heads/main/modernx.lua";
+  #   hash = "sha256-Vfd6bWu8z7RqB8HzuKctxM9/AHAN7/P4KHQ4IRXGx4Y=";
+  # });
 }
