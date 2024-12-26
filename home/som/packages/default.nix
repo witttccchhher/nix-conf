d7c06516e8cf0d57aea6e328f02a7d1743837226
somokill <somokill@noreply.codeberg.org>
2024-12-26 15:58:46 +0500

    Some changes (auto)

--- home/som/packages/default.nix
+++ home/som/packages/default.nix
@@ -22,7 +22,7 @@
     packages = with pkgs; [
       # Flakes
       inputs.zen-browser.packages.${system}.generic
-      inputs.toru.defaultPackage.${system}.default
+      inputs.toru.defaultPackage.${system}.defaultPackage
       (callPackage ./derivations/ani-cli/ani-cli-ru.nix { })
       (callPackage ./derivations/curd/curd.nix { })
 
