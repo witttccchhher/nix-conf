84466c96d5cf405d774dfbed9bea0e3cba745dd0
somokill <somokill@noreply.codeberg.org>
2024-12-30 14:48:47 +0500

    Some changes (auto)

---
+++ home/som/ags/agscher/flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "My Awesome Desktop Shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    astal = {
+      url = "github:aylur/astal";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    ags = {
+      url = "github:aylur/ags";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, astal, ags }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system} = {
+      default = pkgs.mkShell {
+        buildInputs = [
+          astal.packages.${system}.default
+          (ags.packages.${system}.default.override {
+            extraPackages = with ags.packages.${system}; [
+              notifd
+            ];
+          })
+        ];
+      };
+    };
+    packages.${system}. default = pkgs.stdenvNoCC.mkDerivation rec {
+      name = "agscher";
+      src = ./.;
+
+      nativeBuildInputs = [
+        ags.packages.${system}.default
+        pkgs.wrapGAppsHook
+        pkgs.gobject-introspection
+      ];
+
+      buildInputs = with astal.packages.${system}; [
+        astal3
+        io
+        notifd
+      ];
+
+      installPhase = ''
+        mkdir -p $out/bin
+        ags bundle app.ts $out/bin/${name}
+      '';
+    };
+  };
+}
