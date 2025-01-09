ff675e952b09f54a6f8bbcf39e2748f0206e2262
somokill <somokill@noreply.codeberg.org>
2025-01-09 10:27:08 +0500

    Some changes (auto)

--- hosts/nixos/doas/default.nix
+++ hosts/nixos/doas/default.nix
@@ -1,13 +1,40 @@
 { pkgs, ... }: {
-  security.sudo.enable = true;
+  security.sudo-rs = {
+    enable = true;
+    execWheelOnly = true;
+    wheelNeedsPassword = true;
+    extraRules = [
+      {
+        users = [ "som" ];
+        commands = [
+          {
+            command = "awg-quick";
+            options = [ "NOPASSWD" ];
+          }
+          {
+            command = "openvpn";
+            options = [ "NOPASSWD" ];
+          }
+        ];
+      }
+    ];
+  };
   security.doas = {
     enable = true;
     wheelNeedsPassword = true;
     extraRules = [
       {
-        users = [ "wtchr" ];
+        users = [ "som" ];
         keepEnv = true;
-        persist = false;
+        persist = true;
+      }
+      {
+        cmd = "awg-quick";
+        noPass = true;
+      }
+      {
+        cmd = "openvpn";
+        noPass = true;
       }
     ];
   };
