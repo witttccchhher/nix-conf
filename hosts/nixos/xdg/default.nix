71de826c1a97756048551729875dc9ab134350d3
somokill <somokill@noreply.codeberg.org>
2025-01-10 21:27:46 +0500

    Some changes (auto)

--- hosts/nixos/xdg/default.nix
+++ hosts/nixos/xdg/default.nix
@@ -1,10 +1,12 @@
 { pkgs, lib, ... }: {
   xdg.portal = {
     enable = true;
-    wlr.enable = lib.mkForce false;
+    wlr.enable = lib.mkForce true;
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
       xdg-desktop-portal-gnome
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal
       gnome-keyring
     ];
   };
