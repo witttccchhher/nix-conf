078b12bb19bda1c29cab63a69a3dc068ccddbf25
somokill <somokill@noreply.codeberg.org>
2025-01-17 22:27:12 +0500

    Some changes (auto)

--- home/som/stylix/default.nix
+++ home/som/stylix/default.nix
@@ -5,7 +5,7 @@
     autoEnable = false;
 
     image = let
-      input = "${inputs.wallpapers}/girl/girl13.jpg";
+      input = "${inputs.wallpapers}/girl/girl15.png";
       level = 10;
       lum = 1.0;
       preserve = false;
