ada42ad64e5869bf99aba21901630e530d9d32ef
witttccchhher <somokill650@gmail.com>
2025-01-23 19:52:27 +0500

    Some changes (auto)

--- home/som/yazi/default.nix
+++ home/som/yazi/default.nix
@@ -36,14 +36,14 @@
     };
     theme = with config.lib.stylix.colors; {
       manager = {
-        cwd = { fg = "#${base0C}"; bg = "reset"; bold = true; };
+        cwd = { fg = "#${base0E}"; bg = "reset"; bold = true; };
 
         hovered = { fg = "#${base00}"; bg = "#${base0E}"; bold = true; };
-        preview_hovered = { fg = "#${base0D}"; bg = "reset"; };
+        preview_hovered = { fg = "#${base0E}"; bg = "reset"; };
 
-        find_keyword = { fg = "#${base00}"; underline = true; bold = true; };
+        find_keyword = { fg = "#${base04}"; underline = true; bold = true; };
 
-        tab_active = { fg = "#${base00}"; bg = "#${base0C}"; };
+        tab_active = { fg = "#${base00}"; bg = "#${base0E}"; };
         tab_inactive = { fg = "#${base00}"; bg = "#${base04}"; };
 
         border_style = { fg = "#${base00}"; };
@@ -53,19 +53,19 @@
         separator_open = " ";
         separator_close = " ";
 
-        mode_normal = { fg = "#${base00}"; bg = "#${base0C}"; bold = true; };
+        mode_normal = { fg = "#${base00}"; bg = "#${base0E}"; bold = true; };
         mode_select = { fg = "#${base00}"; bg = "#${base0A}"; bold = true; };
         mode_unset = { fg = "#${base00}"; bg = "#${base09}"; bold = true; };
 
-        Progress_label = { fg = "#${base00}"; bg = "#${base0C}"; bold = true; };
-        Progress_normal = { fg = "#${base00}"; bg = "#${base0C}"; bold = true; };
+        Progress_label = { fg = "#${base00}"; bg = "#${base0E}"; bold = true; };
+        Progress_normal = { fg = "#${base00}"; bg = "#${base0E}"; bold = true; };
         Progress_error = { fg = "#${base00}"; bg = "#${base08}"; bold = true; };
       };
 
       filetype = {
         rules = [
-          { mime = "*"; is = "exec"; fg = "#${base0D}"; }
-          { mime = "*"; fg = "#${base07}"; }
+          { mime = "*"; is = "exec"; fg = "#${base0E}"; }
+          { mime = "*"; fg = "#${base05}"; }
         ];
       };
     };
