078e538a1dfa3b481e7aa2f046c478af395526aa
witttccchhher <somokill650@gmail.com>
2025-02-02 21:51:49 +0500

    Some changes (auto)

--- home/som/nixpkgs/default.nix
+++ home/som/nixpkgs/default.nix
@@ -1,14 +1,9 @@
-{ pkgs, ... }: {
+{ ... }: {
   nixpkgs.config = {
     allowUnfree = true;
     allowBroken = true;
     allowAliases = true;
-    permittedInsecurePackages = [
-      "pulsar-1.122.0"
-      "olm-3.2.16"
-    ];
-    allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
-      "beat-prints"
-    ];
+    allowInsecure = true;
+    allowUnfreePredicate = true;
   };
 }
--- hosts/nixos/nixpkgs/default.nix
+++ hosts/nixos/nixpkgs/default.nix
@@ -1,10 +1,9 @@
-{ pkgs, ... }: {
+{ ... }: {
   nixpkgs.config = {
     allowAliases = true;
     allowUnfree = true;
     allowBroken = true;
-    allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
-      "beat-prints"
-    ];
+    allowInsecure = true;
+    allowUnfreePredicate = true;
   };
 }
