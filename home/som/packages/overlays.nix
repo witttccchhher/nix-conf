4dd0791a79067405e5452aa1b86f2bb026e067c0
witttccchhher <somokill650@gmail.com>
2025-02-03 15:01:48 +0500

    Some changes (auto)

--- home/som/packages/overlays.nix
+++ home/som/packages/overlays.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
   nixpkgs.overlays = [
     (self: super: {
       spotify = super.spotify.overrideAttrs (old: let
@@ -6,7 +6,7 @@
         version = "1.2.45.454.gc16ec9f6";
       in rec {
         inherit version;
-        src = builtins.fetchurl {
+        src = pkgs.fetchurl {
           name = "spotify-${version}-${rev}.snap";
           url = "https://api.snapcraft.io/api/v1/snaps/download/pOBIoZ2LrCB3rDohMxoYGnbN14EHOgD7_${rev}.snap";
           hash = "sha512-F1Npz/oKCsMKaQx2M5dm1dhWhaSlt8422tpRWnwuk2yjwLWrOYDY2uKYph8YFXfOdS3mV6u5yVlzgFdDqAFmCQ==";
