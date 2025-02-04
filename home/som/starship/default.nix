2c9708a9755ccfb7d19f4ba04163b4ecc027cca4
witttccchhher <somokill650@gmail.com>
2025-02-04 17:19:50 +0500

    Some changes (auto)

--- home/som/starship/default.nix
+++ home/som/starship/default.nix
@@ -13,6 +13,14 @@
         "$nodejs"
         "$character"
       ];
+      right_format = lib.concatStrings [
+        "$cmd_duration"
+      ];
+      add_newline = true;
+
+      directory = {
+        truncation_length	= 1;
+      };
     };
   };
 }
