2dda889ec597ddff6b2508697caf46593fa595cc
witttccchhher <somokill650@gmail.com>
2025-02-04 07:14:12 +0500

    Some changes (auto)

--- home/som/foot/default.nix
+++ home/som/foot/default.nix
@@ -4,10 +4,10 @@
     package = pkgs.foot;
     settings = {
       main = {
-        font = lib.mkForce "CommitMono Nerd Font:size=12, JetBrainsMono Nerd Font:size=12";
-        font-bold = lib.mkForce "CommitMono Nerd Font:size=12:style=Bold, JetBrainsMono Nerd Font:size=12:style=Bold";
-        font-italic = lib.mkForce "CommitMono Nerd Font:size=12:style=Italic, JetBrainsMono Nerd Font:size=12:style=Italic";
-        font-bold-italic = lib.mkForce "CommitMono Nerd Font:size=12:style=Bold Italic, JetBrainsMono Nerd Font:size=12:style=Bold Italic";
+        font = lib.mkForce "Cascadia Mono NF:size=12";
+        font-bold = lib.mkForce "Cascadia Mono NF:size=12:style=Bold";
+        font-italic = lib.mkForce "Cascadia Mono NF:size=12:style=Italic";
+        font-bold-italic = lib.mkForce "Cascadia Mono NF:size=12:style=Bold Italic";
         pad = "7x7";
         initial-window-mode = "maximized";
         box-drawings-uses-font-glyphs = "no";
