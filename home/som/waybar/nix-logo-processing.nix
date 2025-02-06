fc6058b60ad18b4a1abb62061cf89b94e172e56f
witttccchhher <somokill650@gmail.com>
2025-02-06 21:27:50 +0500

    Some changes (auto)

--- home/som/waybar/nix-logo-processing.nix
+++ home/som/waybar/nix-logo-processing.nix
@@ -16,7 +16,13 @@
     mkdir -p $out
     cp $src/${filename} $out
     chmod +rw $out/${filename}
-    ${pkgs.imagemagick}/bin/magick $out/${filename} -fuzz 15% -fill "${firstColor}" -opaque "#5277c3" $out/${filename}
-    ${pkgs.imagemagick}/bin/magick $out/${filename} -fuzz 15% -fill "${secondColor}" -opaque "#7ebae4" $out/${filename}
+
+    sed -i -e "s/#699ad7/${firstColor}/" $out/${filename}
+    sed -i -e "s/#7eb1dd/${firstColor}/" $out/${filename}
+    sed -i -e "s/#7ebae4/${firstColor}/" $out/${filename}
+
+    sed -i -e "s/#415e9a/${secondColor}/" $out/${filename}
+    sed -i -e "s/#4a6baf/${secondColor}/" $out/${filename}
+    sed -i -e "s/#5277c3/${secondColor}/" $out/${filename}
   '';
 }
