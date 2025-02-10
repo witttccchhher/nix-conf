b30cc1e28bfce78b93ab182144dd638a7ce35a83
witttccchhher <somokill650@gmail.com>
2025-02-10 22:20:10 +0500

    Some changes (auto)

--- home-modules/default.nix
+++ home-modules/default.nix
@@ -4,6 +4,9 @@
 
     exclude = [
       ./default.nix
+      ./custom
+      ./programs/graphical/waybar/google-icons.nix
+      ./programs/graphical/waybar/nix-logo-processing.nix
     ];
   };
 }
