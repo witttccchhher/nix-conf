67b18e8570c6f4ceebe7fcf29fbd63534f2d79e5
witttccchhher <somokill650@gmail.com>
2025-03-16 12:42:47 +0500

    Autocommit by NX

--- home-modules/programs/graphical/hyprland.nix
+++ home-modules/programs/graphical/hyprland.nix
@@ -18,7 +18,7 @@
           hyprspace
       ]);
 
-    extraConfig = with config.lib.stylix.colors; /* toml */ ''
+    extraConfig = with config.lib.stylix.colors; /* hyprlang */ ''
       monitor=,preferred,auto,auto
 
       $terminal = kitty
@@ -94,15 +94,6 @@
         animation = workspacesOut, 1, 1.94, easeInOutCubic, slidefadevert 20%
       }
 
-      dwindle {
-        pseudotile = true
-        preserve_split = true
-      }
-
-      master {
-        new_status = master
-      }
-
       misc {
         force_default_wallpaper = 0
         disable_hyprland_logo = true
@@ -143,28 +134,6 @@
       bind = $mod SHIFT, H, scroller:movewindow, l
       bind = $mod SHIFT, K, movetoworkspace, -1
       bind = $mod SHIFT, J, movetoworkspace, +1
-
-      bind = $mod, 1, workspace, 1
-      bind = $mod, 2, workspace, 2
-      bind = $mod, 3, workspace, 3
-      bind = $mod, 4, workspace, 4
-      bind = $mod, 5, workspace, 5
-      bind = $mod, 6, workspace, 6
-      bind = $mod, 7, workspace, 7
-      bind = $mod, 8, workspace, 8
-      bind = $mod, 9, workspace, 9
-      bind = $mod, 0, workspace, 10
-
-      bind = $mod SHIFT, 1, movetoworkspace, 1
-      bind = $mod SHIFT, 2, movetoworkspace, 2
-      bind = $mod SHIFT, 3, movetoworkspace, 3
-      bind = $mod SHIFT, 4, movetoworkspace, 4
-      bind = $mod SHIFT, 5, movetoworkspace, 5
-      bind = $mod SHIFT, 6, movetoworkspace, 6
-      bind = $mod SHIFT, 7, movetoworkspace, 7
-      bind = $mod SHIFT, 8, movetoworkspace, 8
-      bind = $mod SHIFT, 9, movetoworkspace, 9
-      bind = $mod SHIFT, 0, movetoworkspace, 10
 
       bindm = $mod, mouse:272, movewindow
       bindm = $mod, mouse:273, resizewindow
--- nixos-modules/system/xdg.nix
+++ nixos-modules/system/xdg.nix
@@ -3,9 +3,10 @@
     enable = true;
     wlr.enable = lib.mkForce true;
     extraPortals = with pkgs; [
-      # xdg-desktop-portal-gtk
+      xdg-desktop-portal-gtk
       # xdg-desktop-portal-gnome
-      # xdg-desktop-portal-wlr
+      xdg-desktop-portal-hyprland
+      xdg-desktop-portal-wlr
       xdg-desktop-portal
       gnome-keyring
     ];
