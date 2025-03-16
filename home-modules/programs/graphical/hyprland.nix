8378ba0fa75d08bffda2c1579aa30c6bf58b6673
witttccchhher <somokill650@gmail.com>
2025-03-16 10:15:32 +0500

    Autocommit by NX

--- home-modules/programs/graphical/hyprland.nix
+++ home-modules/programs/graphical/hyprland.nix
@@ -18,7 +18,7 @@
           hyprspace
       ]);
 
-    extraConfig = /* toml */ ''
+    extraConfig = with config.lib.stylix.colors; /* toml */ ''
       monitor=,preferred,auto,auto
 
       $terminal = kitty
@@ -28,108 +28,107 @@
       env = HYPRCURSOR_SIZE,24
 
       general {
-          gaps_in = 10
-          gaps_out = 20
+        gaps_in = 10
+        gaps_out = 20
 
-          border_size = 2
+        border_size = 2
+        col.active_border = rgba(${base0F}aa)
+        col.inactive_border = rgba(${base03}aa)
+        resize_on_border = false
 
-          col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
-          col.inactive_border = rgba(595959aa)
+        allow_tearing = false
 
-          resize_on_border = false
-
-          allow_tearing = false
-
-          layout = scroller
+        layout = scroller
+        no_focus_fallback = true
       }
 
       decoration {
-          rounding = 20
-          rounding_power = 2
+        rounding = 20
+        rounding_power = 2
 
-          active_opacity = 0.9
-          inactive_opacity = 0.8
+        active_opacity = 0.9
+        inactive_opacity = 0.8
 
-          shadow {
-              enabled = true
-              range = 4
-              render_power = 3
-              color = rgba(1a1a1aee)
-          }
+        shadow {
+          enabled = true
+          range = 4
+          render_power = 3
+          color = rgba(1a1a1aee)
+        }
 
-          blur {
-              enabled = true
-              size = 10
-              passes = 1
+        blur {
+          enabled = true
+          size = 10
+          passes = 1
 
-              vibrancy = 0.1696
-          }
+          vibrancy = 0.1696
+        }
       }
 
       animations {
-          enabled = yes, please :)
+        enabled = yes, please :)
 
-          bezier = easeOutQuint,0.23,1,0.32,1
-          bezier = easeInOutCubic,0.65,0.05,0.36,1
-          bezier = linear,0,0,1,1
-          bezier = almostLinear,0.5,0.5,0.75,1.0
-          bezier = quick,0.15,0,0.1,1
+        bezier = easeOutQuint,0.23,1,0.32,1
+        bezier = easeInOutCubic,0.65,0.05,0.36,1
+        bezier = linear,0,0,1,1
+        bezier = almostLinear,0.5,0.5,0.75,1.0
+        bezier = quick,0.15,0,0.1,1
 
-          animation = global, 1, 10, default
-          animation = border, 1, 5.39, easeOutQuint
-          animation = windows, 1, 4.79, easeOutQuint
-          animation = windowsIn, 1, 4.1, easeOutQuint, popin 87%
-          animation = windowsOut, 1, 1.49, linear, popin 87%
-          animation = fadeIn, 1, 1.73, almostLinear
-          animation = fadeOut, 1, 1.46, almostLinear
-          animation = fade, 1, 3.03, quick
-          animation = layers, 1, 3.81, easeOutQuint
-          animation = layersIn, 1, 4, easeOutQuint, fade
-          animation = layersOut, 1, 1.5, linear, fade
-          animation = fadeLayersIn, 1, 1.79, almostLinear
-          animation = fadeLayersOut, 1, 1.39, almostLinear
-          animation = workspaces, 1, 1.94, almostLinear, fade
-          animation = workspacesIn, 1, 1.21, almostLinear, fade
-          animation = workspacesOut, 1, 1.94, almostLinear, fade
+        animation = global, 1, 10, default
+        animation = border, 1, 5.39, easeOutQuint
+        animation = windows, 1, 4.79, easeOutQuint
+        animation = windowsIn, 1, 4.1, easeOutQuint, popin 87%
+        animation = windowsOut, 1, 1.49, linear, popin 87%
+        animation = fadeIn, 1, 1.73, almostLinear
+        animation = fadeOut, 1, 1.46, almostLinear
+        animation = fade, 1, 3.03, quick
+        animation = layers, 1, 3.81, easeOutQuint
+        animation = layersIn, 1, 4, easeOutQuint, fade
+        animation = layersOut, 1, 1.5, linear, fade
+        animation = fadeLayersIn, 1, 1.79, almostLinear
+        animation = fadeLayersOut, 1, 1.39, almostLinear
+        animation = workspaces, 1, 1.94, almostLinear, fade
+        animation = workspacesIn, 1, 1.21, almostLinear, fade
+        animation = workspacesOut, 1, 1.94, almostLinear, fade
       }
 
       dwindle {
-          pseudotile = true
-          preserve_split = true
+        pseudotile = true
+        preserve_split = true
       }
 
       master {
-          new_status = master
+        new_status = master
       }
 
       misc {
-          force_default_wallpaper = 0 # Set to 0 or 1 to disable the anime mascot wallpapers
-          disable_hyprland_logo = true # If true disables the random hyprland logo / anime girl background. :(
+        force_default_wallpaper = 0 # Set to 0 or 1 to disable the anime mascot wallpapers
+        disable_hyprland_logo = true # If true disables the random hyprland logo / anime girl background. :(
       }
 
       input {
-          kb_layout = us,ru
-          kb_variant =
-          kb_model =
-          kb_options = grp:caps_toggle
-          kb_rules =
+        kb_layout = us,ru
+        kb_variant =
+        kb_model =
+        kb_options = grp:caps_toggle
+        kb_rules =
 
-          follow_mouse = 1
+        follow_mouse = 1
 
-          sensitivity = 0
+        sensitivity = 0
 
-          touchpad {
-              natural_scroll = false
-          }
+        touchpad {
+          natural_scroll = false
+        }
       }
 
       gestures {
-          workspace_swipe = false
+        workspace_swipe = false
       }
 
       device {
-          name = epic-mouse-v1
-          sensitivity = -0.5
+        name = epic-mouse-v1
+        sensitivity = -0.5
       }
 
       $mod = SUPER # Sets "Windows" key as main modifier
@@ -147,6 +146,11 @@
       bind = $mod, H, scroller:movefocus, l
       bind = $mod, K, scroller:movefocus, u
       bind = $mod, J, scroller:movefocus, d
+
+      bind = $mod SHIFT, L, scroller:movewindow, r
+      bind = $mod SHIFT, H, scroller:movewindow, l
+      bind = $mod SHIFT, K, scroller:movewindow, u
+      bind = $mod SHIFT, J, scroller:movewindow, d
 
       bind = $mod, 1, workspace, 1
       bind = $mod, 2, workspace, 2
