836df145def170eebb267db3ed027ed30d983e7b
witttccchhher <somokill650@gmail.com>
2025-04-05 20:54:52 +0500

    Autocommit by NX

--- nixos-modules/system/containers.nix
+++ nixos-modules/system/containers.nix
@@ -1,31 +1,3 @@
 { ... }: {
-  containers = {
-    wireguardForTerraria = {
-      autoStart = true;
-      privateNetwork = true;
-      hostAddress = "192.168.100.10";
-      localAddress = "192.168.100.11";
-
-      config = { pkgs, lib, ... }: {
-        environment.systemPackages = with pkgs; [
-          neovim
-          wireguard-go
-          wireguard-tools
-        ];
-
-        system.stateVersion = "25.05";
-
-        networking = {
-          wireless.enable = true;
-          firewall = {
-            enable = true;
-            allowedTCPPorts = [ 80 ];
-            allowedUDPPorts = [ 51820 ];
-          };
-          useHostResolvConf = lib.mkForce false;
-        };
-        services.resolved.enable = true;
-      };
-    };
-  };
+  containers = { };
 }
