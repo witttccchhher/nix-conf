29aec742b0d5882376e3e6a4804f977be8e64c7e
witttccchhher <somokill650@gmail.com>
2025-04-06 20:31:04 +0500

    Autocommit by NX

--- nixos-modules/programs/terminal/fonts.nix
+++ nixos-modules/programs/terminal/fonts.nix
@@ -2,13 +2,14 @@
   fonts = {
     packages = with pkgs; [
       cascadia-code
+      commit-mono
       dejavu_fonts
       (google-fonts.override { fonts = [ "Inter" ]; })
     ];
 
     fontconfig = {
       defaultFonts = {
-        monospace = [ "Cascadia Mono" ];
+        monospace = [ "Commit Mono" ];
         sansSerif = [ "Inter" ];
         serif = [ "Inter" ];
       };
