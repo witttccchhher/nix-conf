bd2a9ac08d534f40fd44f4e9cd04730e801ef1ed
witttccchhher <somokill650@gmail.com>
2025-04-06 09:50:24 +0500

    Autocommit by NX

--- home-modules/programs/graphical/zed.nix
+++ home-modules/programs/graphical/zed.nix
@@ -1,18 +1,12 @@
 { pkgs, lib, ... }: {
   programs.zed-editor = {
     enable = true;
-    package = pkgs.zed-editor-fhs;
+    package = pkgs.zed-editor;
     extensions = [ "make" "nix" ];
     extraPackages = with pkgs; [
       nixd
       gopls
       basedpyright
-
-      cascadia-code
     ];
-
-    userSettings = {
-      buffer_font_family = lib.mkForce "CascadiaMonoNF-Regular";
-    };
   };
 }
