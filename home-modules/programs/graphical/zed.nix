793da7a4f27cdb97408a36e994ba87adfc9dd59e
witttccchhher <somokill650@gmail.com>
2025-04-06 08:26:34 +0500

    Autocommit by NX

--- home-modules/programs/graphical/zed.nix
+++ home-modules/programs/graphical/zed.nix
@@ -1,13 +1,12 @@
 { pkgs, ... }: {
   programs.zed-editor = {
     enable = true;
+    package = pkgs.zed-editor-fhs;
     extensions = [ "make" "nix" ];
     extraPackages = with pkgs; [
       nixd
       gopls
       basedpyright
-      
-      cascadia-code
     ];
   };
 }
