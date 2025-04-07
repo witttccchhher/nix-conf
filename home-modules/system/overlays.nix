08af5da885298c5b88bb779f6c721e3487a98113
witttccchhher <somokill650@gmail.com>
2025-04-07 14:54:55 +0500

    Autocommit by NX

--- home-modules/system/overlays.nix
+++ home-modules/system/overlays.nix
@@ -1,16 +1,18 @@
-{ inputs, pkgs, ... }: {
+{ inputs, pkgs, ... }:
+{
   nixpkgs.overlays = [
     (self: super: {
       zed-editor = super.zed-editor.overrideAttrs (prev: rec {
         version = "0.180.2";
-        
+
         src = prev.src // {
           tag = "v${version}";
           hash = "";
         };
-        
+
         env = prev.env // {
           RELEASE_VERSION = version;
+        };
       });
     })
   ];
