072cbc50f66adc0bc1ab8afaa952806972cf1b99
witttccchhher <somokill650@gmail.com>
2025-04-07 16:49:06 +0500

    Autocommit by NX

--- home-modules/system/overlays.nix
+++ home-modules/system/overlays.nix
@@ -5,10 +5,13 @@
       zed-editor = super.zed-editor.overrideAttrs (prev: rec {
         version = "0.180.2";
 
-        src = prev.src // {
+        src = pkgs.fetchFromGitHub {
+          owner = "zed-industries";
+          repo = "zed";
           tag = "v${version}";
           hash = "";
         };
+        cargoHash = "";
 
         env = prev.env // {
           RELEASE_VERSION = version;
