703e5e8ca0aca61bf0b1ca91c8c2fc85ec8edb36
witttccchhher <somokill650@gmail.com>
2025-06-09 18:49:46 +0500

    Autocommit by NX

--- home-modules/programs/graphical/niri.nix
+++ home-modules/programs/graphical/niri.nix
@@ -1,8 +1,8 @@
 { pkgs, inputs, ... }: {
   imports = [ inputs.niri.homeModules.niri ];
 
-  programs.niri = {
-    enable = true;
-    package = inputs.niri.packages.${pkgs.system}.niri-unstable;
-  };
+  # programs.niri = {
+  #   enable = true;
+  #   package = inputs.niri.packages.${pkgs.system}.niri-unstable;
+  # };
 }
