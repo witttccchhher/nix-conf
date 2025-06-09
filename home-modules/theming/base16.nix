50ac2ba769bfa5652473c3ab2f5bf7a4ab918fa0
witttccchhher <somokill650@gmail.com>
2025-06-09 20:05:05 +0500

    Autocommit by NX

--- home-modules/theming/base16.nix
+++ home-modules/theming/base16.nix
@@ -1,5 +1,5 @@
 { config, ... }: {
-  home.file.".config/stylix/palette.scss" = with config.lib.stylix.colors.withHashtag; ''
+  xdg.configFile."stylix/palette.scss".text = with config.lib.stylix.colors.withHashtag; ''
     $base00: ${base00};
     $base01: ${base01};
     $base02: ${base02};
