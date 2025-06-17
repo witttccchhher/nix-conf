6a8301aebcec46b23ae8c6263e7090715094ed6f
witttccchhher <somokill650@gmail.com>
2025-06-17 13:31:01 +0500

    Autocommit by NX

--- home-modules/programs/graphical/ags/agscher/windows.ts
+++ home-modules/programs/graphical/ags/agscher/windows.ts
@@ -1,14 +1,4 @@
 import NotificationPopups from "./notifications/NotificationPopups";
-import Launcher from "./launcher/Launcher.tsx";
-import Bar from "./bar/Bar.tsx";
-import Menu from "./menu/Menu.tsx";
-import ScreenRecorder from "./menu/popups/ScreenRecorder.tsx";
-import ScreenShoter from "./menu/popups/ScreenShoter.tsx";
+import Launcher from "./launcher/Launcher";
 
-export default [
-  NotificationPopups,
-
-  Launcher,
-
-  Bar,
-];
+export default [NotificationPopups, Launcher];
