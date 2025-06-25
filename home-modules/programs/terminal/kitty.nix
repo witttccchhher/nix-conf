926d37dfc31dfc9facd3fc8b2d241e88b3db348e
witttccchhher <somokill650@gmail.com>
2025-06-25 12:58:29 +0500

    Autocommit by NX

--- home-modules/programs/terminal/kitty.nix
+++ home-modules/programs/terminal/kitty.nix
@@ -80,6 +80,8 @@
       background ${base00}
 
       dim_opacity 0.5
+      background_opacity 0.5
+      background_blur 1
 
       selection_foreground ${base00}
       selection_background ${base07}
