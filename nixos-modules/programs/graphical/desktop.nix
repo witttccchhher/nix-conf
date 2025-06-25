74066c7c433d7ccb7d0332ecd3cd3c1957f4317c
witttccchhher <somokill650@gmail.com>
2025-06-25 12:01:12 +0500

    Autocommit by NX

--- nixos-modules/programs/graphical/desktop.nix
+++ nixos-modules/programs/graphical/desktop.nix
@@ -2,7 +2,31 @@
   imports = [ inputs.niri.nixosModules.niri ];
 
   programs.niri = {
-    enable = true;
+    enable = false;
     package = inputs.niri.packages.${pkgs.system}.niri-unstable;
+  };
+
+  services = {
+    desktopManager.gnome.enable = true;
+    displayManager.gdm = {
+      enable = true;
+      autoSuspend = false;
+      settings = {
+        daemon = {
+          AutomaticLoginEnable = true;
+          AutomaticLogin = "witcher";
+        };
+      };
+    };
+
+    gnome = {
+      enable = true;
+      gnome-settings-daemon.enable = true;
+      gnome-keyring.enable = true;
+      core-shell.enable = true;
+      core-os-services.enable = true;
+      core-developer-tools.enable = true;
+      core-apps.enable = true;
+    };
   };
 }
