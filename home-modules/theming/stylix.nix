d74f4c03fd108a8d60bea1d9bb050a10618ff978
witttccchhher <somokill650@gmail.com>
2025-06-29 12:24:07 +0500

    Autocommit by NX

--- home-modules/theming/stylix.nix
+++ home-modules/theming/stylix.nix
@@ -15,7 +15,8 @@
     enable = true;
     autoEnable = false;
 
-    image = w.wallpaper { colors = config.lib.stylix.colors; };
+    # image = w.wallpaper { colors = config.lib.stylix.colors; };
+    image = w.wallpaper;
     polarity = w.polarity;
     base16Scheme = "${inputs.base16}/colorschemes/${w.colorscheme}.yaml";
 
