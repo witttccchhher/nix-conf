cf721035a6bef9e04273b69c56aeca1f7f2c59de
witttccchhher <somokill650@gmail.com>
2025-07-18 22:45:25 +0500

    Autocommit by NX

--- home-modules/programs/terminal/helix.nix
+++ home-modules/programs/terminal/helix.nix
@@ -11,17 +11,5 @@
 
       nixfmt-rfc-style
     ];
-
-    extraConfig = ''
-      [keys.insert]
-      up = "no_op"
-      down = "no_op"
-      left = "no_op"
-      right = "no_op"
-      pageup = "no_op"
-      pagedown = "no_op"
-      home = "no_op"
-      end = "no_op"
-    '';
   };
 }
