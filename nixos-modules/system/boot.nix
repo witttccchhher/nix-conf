5e881429849ad8f7d3010e04768d3cadf20b72a0
witttccchhher <somokill650@gmail.com>
2025-08-02 12:49:00 +0500

    Autocommit by NX

--- nixos-modules/system/boot.nix
+++ nixos-modules/system/boot.nix
@@ -10,7 +10,7 @@
       "i915.enable_guc=2"
       "i915.enable_fbc=1"
       "i915.enable_psr=2"
-      "radeon.modeset=0"
+      "nomodeset"
     ];
     extraModulePackages = with config.boot.kernelPackages; [
       amneziawg
